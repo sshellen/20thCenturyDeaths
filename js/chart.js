38e8a0adf3619b3de25ebf3a2005a06e6647b6eb
Steinar <sshellen@users.noreply.github.com>
2021-07-15 23:30:11 -0700

    Add files via upload
    Uploading initial files to create repo.

---
+++ js/chart.js
@@ -0,0 +1,140 @@
+const circleChart = () => {
+  const pack = (data) =>
+    d3.pack().size([width, height]).padding(10)(
+      d3
+        .hierarchy(data)
+        .sum((d) => d.value)
+        .sort((a, b) => b.value - a.value)
+    );
+  const width = 990;
+  const height = 990;
+
+  const root = pack(deaths);
+
+  let focus = root;
+  let view;
+  const format = d3.format(",");
+  const color = d3
+    .scaleLinear()
+    .domain([0, 1])
+    .range(["#85aa74", "#a8db91"])
+    .interpolate(d3.interpolateRgb);
+
+  const svg = d3
+    .create("svg")
+    .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+    .style("display", "block")
+    .style("background", color(0))
+    .style("cursor", "pointer")
+    .on("click", (event) => zoom(event, root));
+
+  const node = svg
+    .append("g")
+    .selectAll("circle")
+    .data(root.descendants().slice(1))
+    .join("circle")
+    .attr("fill", (d) => (d.children ? color(d.depth) : "white"))
+    .attr("pointer-events", (d) => (!d.children ? "none" : null))
+
+    .on("mouseover", function () {
+      d3.select(this).attr("stroke", "#44782c");
+    })
+    .on("mouseout", function () {
+      d3.select(this).attr("stroke", null);
+    })
+    .on(
+      "click",
+      (event, d) => focus !== d && (zoom(event, d), event.stopPropagation())
+    );
+
+  const label = svg
+    .append("g")
+    .style("font", "16px helvetica")
+    .style("font-weight", "bold")
+    .style("fill", "#333")
+    .attr("pointer-events", "none")
+    .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(root.descendants())
+    .join("text")
+    .style("fill-opacity", (d) => (d.parent === root ? 1 : 0))
+    .style("display", (d) => (d.parent === root ? "inline" : "none"))
+    .text((d) => d.data.name);
+
+  const amount = svg
+    .append("g")
+    .style("font", "14px helvetica")
+    .style("fill", "#000")
+    .attr("pointer-events", "none")
+    .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(root.descendants())
+    .join("text")
+    .style("fill-opacity", (d) => (d.parent === root ? 1 : 0))
+    .style("display", (d) => (d.parent === root ? "inline" : "none"))
+    .text((d) => d.data.amount);
+
+  zoomTo([root.x, root.y, root.r * 2]);
+
+  function zoomTo(v) {
+    const k = width / v[2];
+
+    view = v;
+
+    label.attr(
+      "transform",
+      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
+    );
+    amount.attr(
+      "transform",
+      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k + 18})`
+    );
+    node.attr(
+      "transform",
+      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
+    );
+    node.attr("r", (d) => d.r * k);
+  }
+
+  function zoom(event, d) {
+    const focus0 = focus;
+
+    focus = d;
+
+    const transition = svg
+      .transition()
+      .duration(event.altKey ? 7500 : 750)
+      .tween("zoom", (d) => {
+        const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+        return (t) => zoomTo(i(t));
+      });
+
+    label
+      .filter(function (d) {
+        return d.parent === focus || this.style.display === "inline";
+      })
+      .transition(transition)
+      .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
+      .on("start", function (d) {
+        if (d.parent === focus) this.style.display = "inline";
+      })
+      .on("end", function (d) {
+        if (d.parent !== focus) this.style.display = "none";
+      });
+
+    amount
+      .filter(function (d) {
+        return d.parent === focus || this.style.display === "inline";
+      })
+      .transition(transition)
+      .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
+      .on("start", function (d) {
+        if (d.parent === focus) this.style.display = "inline";
+      })
+      .on("end", function (d) {
+        if (d.parent !== focus) this.style.display = "none";
+      });
+  }
+
+  return svg.node();
+};
